2e9f9bb40d197a2ed6da548ea791b7312466a948
Zander Santos <zsantos@RRC.ca>
2023-09-21 13:03:02 -0500

    Initial commit: Set up project directory and file structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# [Assignment 2]
+
+## Description
+To gain experience with the different data types in Python through declaring and using them
+
+## Author
+Zander Santos
+
+## Assignment
+Assignment 2 - Introduction to Python: Declaring and Using Variables
---
+++ sdf_assignment_2.py
@@ -0,0 +1,19 @@
+"""
+Description: Introduction to Python: Declaring and Using Variables
+Author: Zander Santos
+Date: September. 21, 2023
+Usage: To gain experience with the different data types on python
+"""
+
+#SIMPLE DATA TYPES
+
+#CALCULATIONS
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
